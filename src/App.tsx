70724cdbd9a87bf77b3a8bdbb0f5db423df8a1e6
DiggesT <Parchomencodan@mail.ru>
2023-02-11 14:27:23 +0700

    Verbum

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  AlignDropdown,
+  BoldButton,
+  CodeFormatButton,
+  Divider,
+  Editor,
+  EditorComposer,
+  InsertLinkButton,
+  ItalicButton,
+  ToolbarPlugin,
+  UnderlineButton,
+} from "verbum";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Verbum locale test</h1>
+      <div>
+        <EditorComposer>
+          <Editor hashtagsEnabled={true} locale="en">
+            <ToolbarPlugin defaultFontSize="20px">
+              <BoldButton />
+              <ItalicButton />
+              <UnderlineButton />
+              <CodeFormatButton />
+              <InsertLinkButton />
+              <Divider />
+              <AlignDropdown />
+            </ToolbarPlugin>
+          </Editor>
+        </EditorComposer>
+      </div>
     </div>
   );
 }
